a0907b246e9b7d4e947f58e8339b63c2dace4824
chenzw <1490210759@qq.com>
2022-05-20 18:19:11 +0800

    feat:添加侧边菜单排序

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -21,4 +21,11 @@ export const parameters = {
       date: /Date$/,
     },
   },
+
+  // 
+  options: {
+    // 排序
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  }
 }
