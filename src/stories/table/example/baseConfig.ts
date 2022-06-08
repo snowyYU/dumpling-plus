6df8aac884dea00df95a7b9e44f35e959d8d341d
snowyyu <731444920@qq.com>
2022-06-08 14:53:26 +0800

    feat:add examples

--- src/stories/table/example/baseConfig.ts
+++ src/stories/table/example/baseConfig.ts
@@ -1,3 +1,13 @@
+/*
+ * @Author: jasper
+ * @Date: 2022-05-27 16:05:45
+ * @LastEditors: jasper
+ * @LastEditTime: 2022-06-07 18:45:28
+ * @FilePath: /dumpling-plus/src/stories/table/example/baseConfig.ts
+ * @Description:
+ *
+ * Copyright (c) 2022 by 公卫区位大数据前端组, All Rights Reserved.
+ */
 /**
  * columns: 定义表格列配置
  * visible: 是否展示
@@ -11,4 +21,10 @@ export const baseColumn = [
   { prop: "name", label: "姓名" },
   { prop: "sex", label: "性别" },
   { prop: "age", label: "年龄" },
+  {
+    prop: "address",
+    width: "100px",
+    label: "地址",
+    "show-overflow-tooltip": true,
+  },
 ];
